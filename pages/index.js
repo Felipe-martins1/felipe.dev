338090bc218b62f2f115ef2f2baf34acdf44a6a1
Felipe <Felipe.dom.martins@gmail.com>
2020-12-16 11:18:03 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 import getUser from '../utils/getUser'
-import Head from 'next/head'
+import PageHead from '../components/PageHead'
 
-import { BiChevronsDown, BiSun, BiMoon } from "react-icons/bi"
-import { AiOutlineStar, AiFillGithub, AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
-import { GoRepo } from "react-icons/go"
+import { BiChevronsDown, BiSun, BiMoon } from 'react-icons/bi'
+import { AiOutlineStar, AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
+import { GoRepo } from 'react-icons/go'
 const Index = (props) => {
     const scroll = () => {
         window.scrollTo(0, 1000, 'smooth')
@@ -33,26 +33,7 @@ const Index = (props) => {
     }
     return (
         <div>
-            <Head>
-                <title>Felipe.dev</title>
-                <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-                <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/manifest.json" />
-                <meta name="msapplication-TileColor" content="#ffffff" />
-                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-                <meta name="theme-color" content="#ffffff"></meta>
-            </Head>
+            <PageHead />
             <div className='color-mode'>
                 <BiSun className='icon sun' onClick={LightcolorMode} /><BiMoon className='icon moon' onClick={DarkcolorMode} />
             </div>
