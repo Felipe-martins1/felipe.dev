9364bad9b73cea79febd19035e849fa259adb259
Felipe Martins <Felipe.dom.martins@gmail.com>
2026-03-14 17:10:55 -0300

    [fix]: url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,8 @@
 import type { MetadataRoute } from "next";
+import { socialsSection } from "@/resources/socials";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.URL ?? "https://felipemartins.dev";
+  const baseUrl = process.env.URL ?? socialsSection.url;
 
   return {
     rules: {
